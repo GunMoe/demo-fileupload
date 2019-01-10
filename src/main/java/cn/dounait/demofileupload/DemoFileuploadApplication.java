14244919d763b4e545ccc6010f718b4e573f2904
GunMoe <i@dounait.cn>
2019-01-10 11:24:20 +0800

    Initial commit

---
+++ src/main/java/cn/dounait/demofileupload/DemoFileuploadApplication.java
@@ -0,0 +1,14 @@
+package cn.dounait.demofileupload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoFileuploadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoFileuploadApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/cn/dounait/demofileupload/DemoFileuploadApplicationTests.java
@@ -0,0 +1,17 @@
+package cn.dounait.demofileupload;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoFileuploadApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
